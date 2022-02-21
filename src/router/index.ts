0c7dc1adcac68919e69b26b6b302623491db29d0
Darlington <fritzdultimate@gmail.com>
2022-02-21 05:29:02 -0800

    corrected index page route error

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import TabsPage from '../views/TabsPage.vue'
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
-        redirect: '/tabs/tab1'
+        redirect: '/tabs/home'
     },
     {
         path: '/tabs/',
@@ -13,7 +13,7 @@ const routes: Array<RouteRecordRaw> = [
         children: [
             {
                 path: '',
-                redirect: '/tabs/tab1'
+                redirect: '/tabs/home'
             },
             {
                 path: 'home',
